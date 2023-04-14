aab5c0feefb8b3e03832d7d8b773e161437e9229
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-04-14 11:55:54 +0900

    update

---
+++ src/MessageApp.java
@@ -0,0 +1,9 @@
+public class MessageApp {
+
+    // formatter : Shift + Alt + F
+    public static void main(String[] args) {
+        System.out.println("こんにちは");
+        System.out.println("よろしくお願いいたします。");
+    }
+
+}
