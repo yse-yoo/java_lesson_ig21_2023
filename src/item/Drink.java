e3a01e20885de3b6374f12197f145319444dc3a1
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-04-14 13:49:36 +0900

    add variable

--- src/item/Drink.java
+++ src/item/Drink.java
@@ -2,4 +2,12 @@ package item;
 
 public class Drink {
 
+    public static void main(String[] args) {
+        String name;
+        int price;
+        int amount;
+        boolean isSale;
+        
+    }
+
 }
