2789dc2a680ea990cf1e3cc34ae70cfe41128aca
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-04-14 14:33:36 +0900

    add User class

---
+++ src/item/User.java
@@ -0,0 +1,22 @@
+package item;
+
+public class User {
+    
+    public static void main(String[] args) {
+        String name;
+        int no;
+        String email;
+        boolean isActive;
+
+        name = "YSE";
+        no = 20230001;
+        email = "yse@test.com";
+        isActive = true;
+
+        System.out.println(name);
+        System.out.println(no);
+        System.out.println(email);
+        System.out.println(isActive);
+    }
+
+}
