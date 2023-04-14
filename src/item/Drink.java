e23b669f10d49618f2c828a41b410c663065f538
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-04-14 13:57:02 +0900

    update

--- src/item/Drink.java
+++ src/item/Drink.java
@@ -3,11 +3,23 @@ package item;
 public class Drink {
 
     public static void main(String[] args) {
+        // 名称
         String name;
+        // 価格
         int price;
+        // 個数
         int amount;
+        // セール品か？
         boolean isSale;
         
+        name = "コーヒー";
+        price = 150;
+        amount = 3;
+
+        System.out.println(name);
+        System.out.println(price);
+        System.out.println(amount);
+
     }
 
 }
