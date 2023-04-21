2c6e887ca1e7ea15e5673268f4d45f079e28717b
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-04-21 09:37:55 +0900

    variable

--- src/sample/Drink.java
+++ src/sample/Drink.java
@@ -2,4 +2,21 @@ package sample;
 
 public class Drink {
     
+    public static void main(String[] args) {
+        // スニペット: syso
+        System.out.println("Drink class");
+        
+        String name;
+        int price;
+        int amount;
+        boolean isSale;
+
+        name = "コーヒー";
+        price = 150;
+        amount = 3;
+        isSale = true;
+
+        
+    }
+
 }
