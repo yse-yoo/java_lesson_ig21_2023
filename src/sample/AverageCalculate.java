0e141b01508df4a3f944a37f8d36b93254c12b01
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-04-21 11:25:42 +0900

    standard input

--- src/sample/AverageCalculate.java
+++ src/sample/AverageCalculate.java
@@ -1,9 +1,16 @@
 package sample;
 
+import java.util.Scanner;
+
 public class AverageCalculate {
     
     public static void main(String[] args) {
-        
+        System.out.println("平均点の計算");    
+
+        Scanner sc = new Scanner(System.in);
+        System.err.println("国語の点数を入力してください");
+        int japanese = sc.nextInt();
+
     }
 
 }
