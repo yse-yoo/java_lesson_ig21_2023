91fc1dfdbf7f57412208ce8706ca63683616c647
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-04-21 10:28:49 +0900

    update claculate

--- src/sample/ItemCalculate.java
+++ src/sample/ItemCalculate.java
@@ -4,9 +4,19 @@ public class ItemCalculate {
 
     public static void main(String[] args) {
         int answer;
-        answer = 1 + 2;
-        answer = answer + 3;
+        // 2項演算
+        // answer = 1 + 2;
+        // answer = answer + 3;
+        // answer = answer - 2;
+        // answer = answer * 5;
+        // answer = answer / 2;
 
+        // 複合演算
+        answer = 1 + 2;
+        answer += 3;
+        answer -= 2;
+        answer *= 5;
+        answer /= 2;
         System.out.println(answer);
     }
     
