f386afea288b8ef10dfedbf3367bab01b5714e59
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-04-21 11:49:16 +0900

    update

--- src/sample/AverageCalculate.java
+++ src/sample/AverageCalculate.java
@@ -8,9 +8,24 @@ public class AverageCalculate {
         System.out.println("平均点の計算");    
 
         Scanner sc = new Scanner(System.in);
+
         System.err.println("国語の点数を入力してください");
         int japanese = sc.nextInt();
 
+        System.err.println("算数の点数を入力してください");
+        int mathmatic = sc.nextInt();
+
+        System.err.println("理科の点数を入力してください");
+        int science = sc.nextInt();
+
+        System.err.println("社会の点数を入力してください");
+        int society = sc.nextInt();
+
+        // 平均点（average）
+        float average = (japanese + mathmatic + science + society) / 4.0f;
+
+        String message = "平均点は" + average + "です。";
+        System.out.println(message);
     }
 
 }
