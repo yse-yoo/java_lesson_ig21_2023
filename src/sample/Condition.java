5f78037b5dc9d31daa4524fd9a38b25c61de9e90
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-04-21 14:39:42 +0900

    update

--- src/sample/Condition.java
+++ src/sample/Condition.java
@@ -1,14 +1,24 @@
 package sample;
 
 public class Condition {
-    
-    public static void main(String[] args) {
-       // 価格
-       int price = 200; 
-       // 所持金
-       int money = 100;    
 
-        
+    public static void main(String[] args) {
+        // 価格
+        int price = 200;
+        // 所持金
+        int money = 100;
+        // メッセージ
+        String message = "";
+
+        if (price <= 0) {
+
+        } else if (money >= price) {
+            message = "お買い上げありがとうございます。";
+        } else {
+            message = "所持金が足りません。";
+        }
+
+        System.out.println(message);
     }
 
 }
