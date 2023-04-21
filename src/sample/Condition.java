a2cefb305dcc619a35b7562ff935e755c401aaf5
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-04-21 14:14:01 +0900

    add Condition Class

---
+++ src/sample/Condition.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class Condition {
+    
+    public static void main(String[] args) {
+       // 価格
+       int price = 200; 
+       // 所持金
+       int money = 100;    
+
+        
+    }
+
+}
