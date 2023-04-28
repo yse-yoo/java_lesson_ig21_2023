8cb737ee4747d2fbac62874e70233601f0f10aad
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-04-28 10:52:46 +0900

    add judge

--- src/sample/RspGame.java
+++ src/sample/RspGame.java
@@ -6,11 +6,22 @@ public class RspGame {
         final int ROCK = 0;
         final int SCISSORS = 1;
         final int PAPER = 2;
-        int playerHand;
-        int pcHand;
-        int result;
+        int playerHand = ROCK;
+        int pcHand = ROCK;
+        int result = 0;
         String message = "";
 
+        // じゃんけんのジャッジ
+        result = (playerHand + 3 - pcHand) % 3;
+        // じゃんけんの結果
+        if (result == 0) {
+            message = "Draw!";
+        } else if (result == 1) {
+            message = "You Lose...";
+        } else {
+            message = "You Win!!";
+        }
+        System.out.println(message);
     }
 
 }
