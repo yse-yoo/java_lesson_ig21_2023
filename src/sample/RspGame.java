c5e1476b2ff308db73e272767e3b0d792aac70d3
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-04-28 10:32:11 +0900

    add RspGame

---
+++ src/sample/RspGame.java
@@ -0,0 +1,16 @@
+package sample;
+
+public class RspGame {
+    
+    public static void main(String[] args) {
+        final int ROCK = 0;
+        final int SCISSORS = 1;
+        final int PAPER = 2;
+        int playerHand;
+        int pcHand;
+        int result;
+        String message = "";
+
+    }
+
+}
