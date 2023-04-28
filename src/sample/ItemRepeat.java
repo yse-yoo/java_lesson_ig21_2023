c978c45730213d314820c37c631dd501f248f17b
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-04-28 14:14:56 +0900

    update

--- src/sample/ItemRepeat.java
+++ src/sample/ItemRepeat.java
@@ -3,9 +3,18 @@ package sample;
 public class ItemRepeat {
     
     public static void main(String[] args) {
-        //for
+        //fori
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
+        }
+
+        //forのネスト
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                String plot = "(" + i + ", " + j + ")";
+                System.out.print(plot);
+            }
+            System.out.println("");
         }
     }
 
