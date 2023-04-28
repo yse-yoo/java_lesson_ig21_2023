bf245475cc2274d240224bcbb027b185582a8c06
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-04-28 09:16:27 +0900

    update

--- src/sample/Condition.java
+++ src/sample/Condition.java
@@ -4,14 +4,14 @@ public class Condition {
 
     public static void main(String[] args) {
         // 価格
-        int price = 200;
+        int price = -200;
         // 所持金
-        int money = 100;
+        int money = 1000;
         // メッセージ
         String message = "";
 
         if (price <= 0) {
-
+            message = "価格が正しくありません。";
         } else if (money >= price) {
             message = "お買い上げありがとうございます。";
         } else {
