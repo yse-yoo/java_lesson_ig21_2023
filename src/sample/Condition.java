ed3efdd38bb792fc940a9bd20b30e038b7d628a0
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-04-28 09:24:48 +0900

    add maxPrice

--- src/sample/Condition.java
+++ src/sample/Condition.java
@@ -4,13 +4,15 @@ public class Condition {
 
     public static void main(String[] args) {
         // 価格
-        int price = -200;
+        int price = 100;
+        // 最大価格
+        int maxPrice = 10000000;
         // 所持金
         int money = 1000;
         // メッセージ
         String message = "";
 
-        if (price <= 0) {
+        if (price <= 0 || price >= maxPrice) {
             message = "価格が正しくありません。";
         } else if (money >= price) {
             message = "お買い上げありがとうございます。";
