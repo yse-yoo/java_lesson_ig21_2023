4aeca5ec47df33fadf02b0b445e2c6714feeb19c
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-04-28 13:41:07 +0900

    add for statement

--- src/sample/ItemRepeat.java
+++ src/sample/ItemRepeat.java
@@ -3,7 +3,10 @@ package sample;
 public class ItemRepeat {
     
     public static void main(String[] args) {
-        
+        //for
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 
 }
