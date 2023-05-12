6d3ddb36e6515361d8001bd759df5d156d5dcd1b
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-12 09:33:42 +0900

    update

--- src/questions/Lesson4.java
+++ src/questions/Lesson4.java
@@ -30,5 +30,12 @@ public class Lesson4 {
         }
         System.out.println(year + "年");
 
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                System.out.print(i * j + " ");
+            }
+            System.out.println();
+        }
     }
+
 }
