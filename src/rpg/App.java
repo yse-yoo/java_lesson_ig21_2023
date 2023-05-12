ba303af876afbecc0b3f52cebd362314ee0232b1
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-12 11:29:43 +0900

    add new Character

--- src/rpg/App.java
+++ src/rpg/App.java
@@ -3,7 +3,8 @@ package rpg;
 public class App {
     
     public static void main(String[] args) {
-        
+        Character character = new Character();
+        System.out.println(character);
     }
 
 }
