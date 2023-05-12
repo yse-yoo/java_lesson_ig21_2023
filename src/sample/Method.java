1c691e836d62e958b265ca092f46f2c747f4d75c
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-12 10:05:53 +0900

    static method

---
+++ src/sample/Method.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class Method {
+    
+    public static void main(String[] args) {
+
+    }
+
+    // public_static_method
+    public static Float calculate(Float x) {
+        Float y = x + 5;    
+        return y;
+    }
+
+}
