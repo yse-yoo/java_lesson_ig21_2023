9e6cf132695afd51ae3a1eccad86684d93644c70
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-12 10:22:38 +0900

    add calculate total price

--- src/sample/Method.java
+++ src/sample/Method.java
@@ -6,6 +6,9 @@ public class Method {
         Float answer;
         answer = calculate(2.f);
         System.out.println(answer);
+
+        // non static method(class method) は直接呼び出せない
+        // answer = calculate2(2.f);
     }
 
     // public_static_method
@@ -14,4 +17,14 @@ public class Method {
         return y;
     }
 
+    // class method(non-staic method)
+    public Float calculate2(Float x) {
+        Float y = x + 5;    
+        return y;
+    }
+
+    public static int calculateTotalPrice(int price, int amount) {
+        int totalPrice = price * amount;
+        return totalPrice; 
+    }
 }
