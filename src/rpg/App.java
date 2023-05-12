60d3ba21d9ecc96a58ce9dadc54ba1c575461294
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-12 11:44:05 +0900

    instance

--- src/rpg/App.java
+++ src/rpg/App.java
@@ -3,8 +3,17 @@ package rpg;
 public class App {
     
     public static void main(String[] args) {
-        Character character = new Character();
-        System.out.println(character);
+        Character wizard = new Character();
+        System.out.println(wizard);
+
+        wizard.name = "アリス";
+        wizard.job = "Wizard";
+        wizard.hp = 30;
+
+        System.out.println(wizard.name);
+        System.out.println(wizard.job);
+        System.out.println(wizard.hp);
+
     }
 
 }
--- src/rpg/Character.java
+++ src/rpg/Character.java
@@ -1,5 +1,9 @@
 package rpg;
 
 public class Character {
-    
+    // field(property)    
+    String name;
+    String job;
+    int hp;
+
 }
