8504273e181207e5a45019105bdef10ecd71d33e
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-12 14:15:20 +0900

    update

--- src/zoo/Animal.java
+++ src/zoo/Animal.java
@@ -1,5 +1,23 @@
 package zoo;
 
 public class Animal {
-    
+    // property
+    public String type;
+    public String name;
+    public String crying;
+
+    // method
+    public void walk() {
+        String message = this.name + "が歩いた";
+        System.out.println(message);
+    }
+
+    public void cry() {
+        System.out.println(this.crying);
+    }
+
+    public void escape() {
+        String message = this.name + "が逃げた";
+        System.out.println(message);
+    }
 }
--- src/zoo/AnimalApp.java
+++ src/zoo/AnimalApp.java
@@ -1,9 +1,22 @@
 package zoo;
 
 public class AnimalApp {
-    
+
     public static void main(String[] args) {
-        
+        Animal dog = new Animal();
+        dog.type = "dog";
+        dog.name = "ポチ";
+        dog.crying = "わん！";
+
+        Animal cat = new Animal();
+        cat.type = "cat";
+        cat.name = "ミケ";
+        cat.crying = "にゃー！";
+
+        dog.walk();
+        dog.cry();
+        cat.cry();
+        cat.escape();
     }
 
 }
