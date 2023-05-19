eeea0c48ba425156e43457aefe05f7ac301d3505
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-19 11:55:41 +0900

    update

--- src/zoo/App2.java
+++ src/zoo/App2.java
@@ -2,6 +2,15 @@ package zoo;
 
 public class App2 {
     public static void main(String[] args) {
-        
+        Cat cat = new Cat("ミケ");
+        Dog dog = new Dog("ポチ");
+        cat.cry();
+        dog.cry();
+
+        Animal[] animals = { cat, dog };
+        for (Animal animal : animals) {
+            System.out.println(animal.name);
+            animal.cry();
+        }
     }
 }
