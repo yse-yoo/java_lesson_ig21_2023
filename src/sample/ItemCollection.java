107e1c2ad84d13e43fcc1122f7f030b1c82354b8
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-19 13:20:45 +0900

    add ItemCollection

---
+++ src/sample/ItemCollection.java
@@ -0,0 +1,13 @@
+package sample;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ItemCollection {
+    
+    public static void main(String[] args) {
+        List<String> drinks = Arrays.asList("コーヒー", "紅茶", "ほうじ茶");    
+        System.out.println(drinks);
+    }
+
+}
