83d5c63eb541f470a53cdfb9cc1a2871fa86b28d
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-19 11:17:55 +0900

    add foreach

--- src/rpg/App3.java
+++ src/rpg/App3.java
@@ -4,9 +4,21 @@ public class App3 {
     
     public static void main(String[] args) {
         Wizard wizard1 = new Wizard("アリス");
-        Wizard wizard2 = new Wizard("アリス");
+        Wizard wizard2 = new Wizard("テリー");
         Warrior warrior1 = new Warrior("ボブ");
 
+        Character[] characters = {
+            wizard1,
+            wizard2,
+            warrior1,
+        };
+        Monster monster1 = new Monster();
+
+        for (Character character : characters) {
+            System.out.println(character.name + "のこうげき！");
+            character.attack(monster1);
+            System.out.println(monster1.hp);
+        }
 
     }
 
