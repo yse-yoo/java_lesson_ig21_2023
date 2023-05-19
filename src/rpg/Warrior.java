810ef50dea67ac24a96baa7ce99693b5aa1017fe
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-19 09:55:33 +0900

    add Warrior

---
+++ src/rpg/Warrior.java
@@ -0,0 +1,14 @@
+package rpg;
+
+public class Warrior extends Character{
+
+    public Warrior(String name) {
+        super(name);
+        this.job = "warrior";
+        this.hp = 50;
+        this.mp = 0;
+        this.attackPower = 7;
+        this.defencePower = 3;
+    }
+    
+}
