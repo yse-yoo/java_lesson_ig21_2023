701fa0b3d6af9fd254c54f112c7585abe7612b51
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-19 09:45:41 +0900

    add wizard

--- src/rpg/Wizard.java
+++ src/rpg/Wizard.java
@@ -1,5 +1,14 @@
 package rpg;
 
-public class Wizard {
+public class Wizard extends Character {
+
+    public Wizard(String name) {
+        super(name);
+        this.job = "wizard";
+        this.hp = 30;
+        this.mp = 20;
+        this.attackPower = 3;
+        this.defencePower = 2;
+    }
     
 }
