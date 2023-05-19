73b258c7a3042282137d3faa337d76d12c4f54e8
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-19 13:36:09 +0900

    add & set

--- src/sample/ItemCollection.java
+++ src/sample/ItemCollection.java
@@ -5,15 +5,22 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ItemCollection {
-    
+
     public static void main(String[] args) {
-        List<String> drinks = Arrays.asList("コーヒー", "紅茶", "ほうじ茶");    
+        List<String> drinks = Arrays.asList("コーヒー", "紅茶", "ほうじ茶");
         System.out.println(drinks);
 
-       ArrayList<String> drinkList = new ArrayList<>();  
-       drinkList.add("コーヒー");
-       drinkList.add("紅茶");
-       drinkList.add("ほうじ茶");
+        ArrayList<String> drinkList = new ArrayList<>();
+        drinkList.add("コーヒー");
+        drinkList.add("紅茶");
+        drinkList.add("ほうじ茶");
+        System.out.println(drinkList);
+
+        String selectDrink = drinkList.get(1);
+        System.out.println(selectDrink);
+
+        drinkList.set(1, "オレンジジュース");
+        System.out.println(drinkList);
 
     }
 
