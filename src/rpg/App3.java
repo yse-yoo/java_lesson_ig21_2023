9390f3917daf5285571503439f6d8eddcb1c4963
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-19 11:09:08 +0900

    add App3

---
+++ src/rpg/App3.java
@@ -0,0 +1,13 @@
+package rpg;
+
+public class App3 {
+    
+    public static void main(String[] args) {
+        Wizard wizard1 = new Wizard("アリス");
+        Wizard wizard2 = new Wizard("アリス");
+        Warrior warrior1 = new Warrior("ボブ");
+
+
+    }
+
+}
