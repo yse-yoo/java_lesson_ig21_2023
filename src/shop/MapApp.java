a0c58199e5032c54fbdadc3fc66d3588c295c7b4
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-26 09:57:41 +0900

    add Map Data

--- src/shop/MapApp.java
+++ src/shop/MapApp.java
@@ -1,9 +1,16 @@
 package shop;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MapApp {
     
     public static void main(String[] args) {
-        
+        // Ctrl + . で Quick Fix
+        Map<String, String> drinkMap = new HashMap<>();
+        drinkMap.put("D0001", "コーヒー");
+        drinkMap.put("D0002", "紅茶");
+        drinkMap.put("D0003", "ほうじ茶");
     }
 
 }
