37e31b7868f116affd9e757d23f3c4a3fa1bf117
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-26 13:21:23 +0900

    update

--- src/sns/entity/App.java
+++ src/sns/entity/App.java
@@ -5,6 +5,12 @@ public class App {
     public static void main(String[] args) {
         User user = new User();    
         user.setEmail("yse@test.com");
+        user.setName("東京　太郎");
+        user.setPassword("xxxxxxxx");
+
+        System.out.println(user.getEmail());
+        System.out.println(user.getName());
+        System.out.println(user.getPassword());
     }
 
 }
