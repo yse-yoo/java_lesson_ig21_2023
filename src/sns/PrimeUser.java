f7882152e0c377d8691c3461e84375a327e23709
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-26 13:15:01 +0900

    update

--- src/sns/PrimeUser.java
+++ src/sns/PrimeUser.java
@@ -5,6 +5,6 @@ import sns.entity.User;
 public class PrimeUser extends User {
     
     public void showEmail() {
-       System.out.println(this.email); 
+       System.out.println(this.getEmail()); 
     }
 }
--- src/sns/entity/App.java
+++ src/sns/entity/App.java
@@ -4,7 +4,7 @@ public class App {
     
     public static void main(String[] args) {
         User user = new User();    
-        user.email = "yse@test.com";
+        user.setEmail("yse@test.com");
     }
 
 }
