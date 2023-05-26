e40cd8d9487bbdc8cd25ead1c804f52a23ed11a1
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-26 11:08:58 +0900

    update

--- src/shop/App.java
+++ src/shop/App.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 public class App {
     public static void main(String[] args) {
         ArrayList<Drink> drinkList = new ArrayList<>();
-
         Drink drink1 = new Drink("コーヒー", 350, 100);
         Drink drink2 = new Drink("紅茶", 400, 10);
         Drink drink3 = new Drink("ほうじ茶", 300, 50);
--- src/shop/IteratorApp.java
+++ src/shop/IteratorApp.java
@@ -1,9 +1,20 @@
 package shop;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class IteratorApp {
     
     public static void main(String[] args) {
-                
+        ArrayList<Drink> drinkList = new ArrayList<>();
+        Drink drink1 = new Drink("コーヒー", 350, 100);
+        Drink drink2 = new Drink("紅茶", 400, 10);
+        Drink drink3 = new Drink("ほうじ茶", 300, 50);
+
+        drinkList.add(drink1);
+        drinkList.add(drink2);
+        drinkList.add(drink3);
+        
     }
 
 }
