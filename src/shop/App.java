dcd30aafba76a5727c69a0029dd15dd415055c42
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-26 09:26:55 +0900

    update

--- src/shop/App.java
+++ src/shop/App.java
@@ -13,5 +13,24 @@ public class App {
         drinkList.add(drink1);
         drinkList.add(drink2);
         drinkList.add(drink3);
+
+        // 紅茶を取得
+        Drink selectDrink = drinkList.get(1);
+        System.out.println(selectDrink.name);
+        System.out.println(selectDrink.price);
+        System.out.println(selectDrink.stock);
+
+        // 商品追加
+        drinkList.add(new Drink("コーラ", 300, 100));
+
+        // 商品情報更新
+        drinkList.set(0, new Drink("アイスコーヒー", 250, 50));
+
+        // 商品削除
+        drinkList.remove(2);
+
+        for (Drink drink : drinkList) {
+            System.out.println(drink.name);            
+        }
     }
 }
