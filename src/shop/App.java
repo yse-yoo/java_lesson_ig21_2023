b49322cbb8c617b7e7606e619bfacbce8b051091
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-26 09:16:10 +0900

    update DrinkLIst

--- src/shop/App.java
+++ src/shop/App.java
@@ -1,7 +1,17 @@
 package shop;
 
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) {
-        
+        ArrayList<Drink> drinkList = new ArrayList<>();
+
+        Drink drink1 = new Drink("コーヒー", 350, 100);
+        Drink drink2 = new Drink("紅茶", 400, 10);
+        Drink drink3 = new Drink("ほうじ茶", 300, 50);
+
+        drinkList.add(drink1);
+        drinkList.add(drink2);
+        drinkList.add(drink3);
     }
 }
--- src/shop/Drink.java
+++ src/shop/Drink.java
@@ -2,4 +2,14 @@ package shop;
 
 public class Drink {
     
+    public String name;
+    public int price;
+    public int stock;
+
+    public Drink(String name, int price, int stock) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+    
 }
