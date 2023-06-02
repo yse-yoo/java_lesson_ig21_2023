c3829d75a3f7933e3dfe0ddd4e089c147ea3f566
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-02 11:19:06 +0900

    update

--- src/rpg/EquipApp.java
+++ src/rpg/EquipApp.java
@@ -1,9 +1,13 @@
 package rpg;
 
+import rpg.item.Item;
+
 public class EquipApp {
     
     public static void main(String[] args) {
         Wizard wizard1 = new Wizard("アリス");
+        Item weapon = new Item("ナイフ", 300, 5, 0);
+        Item armor = new Item("布の服", 200, 0, 3);
     }
 
 }
--- src/rpg/item/Item.java
+++ src/rpg/item/Item.java
@@ -1,7 +1,14 @@
 package rpg.item;
 
 public class Item {
-    
+
+    public Item(String name, int price, int attackPower, int defencePower) {
+        this.name = name;
+        this.price = price;
+        this.attackPower = attackPower;
+        this.defencePower = defencePower;
+    }
+
     private String name;
     private int price;
     private int attackPower;
