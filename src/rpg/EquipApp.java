249ae3525931ce0a9f9e43e6d36b394b19cbd3f0
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-02 11:21:07 +0900

    update

--- src/rpg/EquipApp.java
+++ src/rpg/EquipApp.java
@@ -8,6 +8,19 @@ public class EquipApp {
         Wizard wizard1 = new Wizard("アリス");
         Item weapon = new Item("ナイフ", 300, 5, 0);
         Item armor = new Item("布の服", 200, 0, 3);
+
+        System.out.println(wizard1.name);
+        System.out.println("--- before ---");
+        System.out.println(wizard1.attackPower);
+        System.out.println(wizard1.defencePower);
+
+        wizard1.equip(weapon);
+        wizard1.equip(armor);
+
+        System.out.println("--- after ---");
+        System.out.println(wizard1.attackPower);
+        System.out.println(wizard1.defencePower);
+
     }
 
 }
