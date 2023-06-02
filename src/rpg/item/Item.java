485003cfee76ba6a2c0bf0505552bdbf11094fd2
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-02 10:57:52 +0900

    add Item

---
+++ src/rpg/item/Item.java
@@ -0,0 +1,37 @@
+package rpg.item;
+
+public class Item {
+    
+    private String name;
+    private int price;
+    private int attackPower;
+    private int defencePower;
+
+    // accessor method
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getPrice() {
+        return price;
+    }
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    public int getAttackPower() {
+        return attackPower;
+    }
+    public void setAttackPower(int attackPower) {
+        this.attackPower = attackPower;
+    }
+    public int getDefencePower() {
+        return defencePower;
+    }
+    public void setDefencePower(int defencePower) {
+        this.defencePower = defencePower;
+    }
+
+    
+}
