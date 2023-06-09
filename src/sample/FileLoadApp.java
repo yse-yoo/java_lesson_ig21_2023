e7755972501d47595024a91e7e67c759b193515d
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-09 13:25:04 +0900

    add FileInputStream

--- src/sample/FileLoadApp.java
+++ src/sample/FileLoadApp.java
@@ -1,9 +1,19 @@
 package sample;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class FileLoadApp {
     
     public static void main(String[] args) {
-        
+        String path = "data/sample.txt";
+        try {
+            FileInputStream file = new FileInputStream(path);
+            System.out.println("ファイルを開きました。");
+        } catch (FileNotFoundException e) {
+            System.out.println("ファイルが開けませんでした。");
+        }
+
     }
 
 }
