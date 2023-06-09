d6ea93af5c0cf8a082a2cda4f3e34f65e9b5d395
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-09 14:32:56 +0900

    add item.csv

---
+++ src/rpg/ItemListApp.java
@@ -0,0 +1,10 @@
+package rpg;
+
+import rpg.item.ItemList;
+
+public class ItemListApp {
+    
+    public static void main(String[] args) {
+        ItemList itemList = new ItemList();
+    }
+}
--- src/rpg/item/ItemList.java
+++ src/rpg/item/ItemList.java
@@ -6,4 +6,12 @@ public class ItemList {
     
     ArrayList<Item> list = new ArrayList<>();
 
+    public ItemList() {
+        this.loadCSV();
+    }
+
+    public void loadCSV() {
+       System.out.println("CSV読み込み"); 
+    }    
+
 }
