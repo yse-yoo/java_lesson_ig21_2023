e28d13c8ff9811af756a96830333a6db9575ae06
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-09 13:29:36 +0900

    add input stream reader

--- src/sample/FileLoadApp.java
+++ src/sample/FileLoadApp.java
@@ -2,6 +2,7 @@ package sample;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
 
 public class FileLoadApp {
     
@@ -9,7 +10,10 @@ public class FileLoadApp {
         String path = "data/sample.txt";
         try {
             FileInputStream file = new FileInputStream(path);
+            InputStreamReader reader = new InputStreamReader(file, "UTF-8");
             System.out.println("ファイルを開きました。");
+
+            
         } catch (FileNotFoundException e) {
             System.out.println("ファイルが開けませんでした。");
         }
