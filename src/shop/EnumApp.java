67b71811589a0b3febadf494e6d389c06c55f846
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-09 10:56:03 +0900

    update

--- src/shop/EnumApp.java
+++ src/shop/EnumApp.java
@@ -3,6 +3,14 @@ package shop;
 public class EnumApp {
     
     public static void main(String[] args) {
+        Drink coffee1 = new Drink("コーヒー", 300, Size.SMALL);
+        Drink coffee2 = new Drink("コーヒー", 350, Size.MEDIUM);
+        Drink coffee3 = new Drink("コーヒー", 400, Size.LARGE);
+
+        System.out.println(coffee1.size);
+        System.out.println(coffee2.size);
+        System.out.println(coffee3.size);
+
         
     }
 
