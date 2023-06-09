c238a25086a78b060f26a54a8fe71af8ec4c0457
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-09 13:39:18 +0900

    update

--- src/sample/FileLoadApp.java
+++ src/sample/FileLoadApp.java
@@ -1,7 +1,9 @@
 package sample;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class FileLoadApp {
@@ -13,9 +15,17 @@ public class FileLoadApp {
             InputStreamReader reader = new InputStreamReader(file, "UTF-8");
             System.out.println("ファイルを開きました。");
 
-            
+            BufferedReader buffer = new BufferedReader(reader);
+
+            String line;
+            while ((line = buffer.readLine()) != null) {
+                System.out.println(line);
+            }
+
         } catch (FileNotFoundException e) {
             System.out.println("ファイルが開けませんでした。");
+        } catch (IOException e) {
+            System.out.println("ファイル読み込みエラー");
         }
 
     }
