5e07f96a51cf94cdba127bcff40731af17a1e2aa
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-16 09:24:16 +0900

    update

--- src/rpg/item/ItemList.java
+++ src/rpg/item/ItemList.java
@@ -1,9 +1,15 @@
 package rpg.item;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
 import java.util.ArrayList;
 
 public class ItemList {
     
+    private final String CSV_PATH = "data/item.csv";
     ArrayList<Item> list = new ArrayList<>();
 
     public ItemList() {
@@ -12,6 +18,11 @@ public class ItemList {
 
     public void loadCSV() {
        System.out.println("CSV読み込み"); 
+       FileInputStream file;
+       InputStreamReader reader;
+       BufferedReader buffer;
+
+       
     }    
 
 }
