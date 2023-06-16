9bf1b6de5f6baf658b05b0bf4c7d2aa7eedc68c6
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-16 09:30:10 +0900

    add fileinputstream

--- src/rpg/item/ItemList.java
+++ src/rpg/item/ItemList.java
@@ -2,13 +2,14 @@ package rpg.item;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.Buffer;
 import java.util.ArrayList;
 
 public class ItemList {
-    
+
     private final String CSV_PATH = "data/item.csv";
     ArrayList<Item> list = new ArrayList<>();
 
@@ -17,12 +18,18 @@ public class ItemList {
     }
 
     public void loadCSV() {
-       System.out.println("CSV読み込み"); 
-       FileInputStream file;
-       InputStreamReader reader;
-       BufferedReader buffer;
+        System.out.println("CSV読み込み");
+        FileInputStream file;
+        InputStreamReader reader;
+        BufferedReader buffer;
 
-       
-    }    
+        // ファイルを開く
+        try {
+            file = new FileInputStream(CSV_PATH);
+        } catch (FileNotFoundException e) {
+            System.out.println(CSV_PATH + " が開けません");
+        }
+
+    }
 
 }
