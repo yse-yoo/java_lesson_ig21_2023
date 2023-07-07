2f8ac5fab3d54d849d8a4f3c4f671881d781d6b9
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-07-07 11:59:59 +0900

    update

--- src/timer/TimerApp.java
+++ src/timer/TimerApp.java
@@ -1,5 +1,8 @@
 package timer;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class TimerApp {
     public static int seconds = 3;
     
@@ -7,6 +10,19 @@ public class TimerApp {
         String message = seconds + "秒後に実行します";
         System.out.println(message);
 
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("実行しました！");
+                timer.cancel();
+            }
+        };
+
+        // msに変換
+        int ms = seconds * 1000;
+        // タイマー実行
+        timer.schedule(task, ms);
     }
 
 }
