974d5682558665e0c28c2190982082e3b6580db4
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-07-07 10:06:32 +0900

    update

--- src/thread/ShopThread.java
+++ src/thread/ShopThread.java
@@ -1,5 +1,16 @@
 package thread;
 
-public class ShopThread {
+public class ShopThread extends Thread {
+
+    private Item item;
+
+    //Constructor
+    ShopThread(Item item) {
+        this.item = item;
+    }
     
+    @Override
+    public void run() {
+        item.order();
+    }
 }
