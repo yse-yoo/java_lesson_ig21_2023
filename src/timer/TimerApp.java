ff4df601866c73f59a454dd78acda4b8579dbff9
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-07-07 11:50:13 +0900

    update

--- src/timer/TimerApp.java
+++ src/timer/TimerApp.java
@@ -1,9 +1,12 @@
 package timer;
 
 public class TimerApp {
+    public static int seconds = 3;
     
     public static void main(String[] args) {
-        
+        String message = seconds + "秒後に実行します";
+        System.out.println(message);
+
     }
 
 }
