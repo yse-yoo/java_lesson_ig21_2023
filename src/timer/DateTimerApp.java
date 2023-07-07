20c436b4d5198ba2c0f195d0c7e78739bbf8e6a6
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-07-07 13:17:41 +0900

    update

--- src/timer/DateTimerApp.java
+++ src/timer/DateTimerApp.java
@@ -2,24 +2,36 @@ package timer;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class DateTimerApp {
-    
+
     public static void main(String[] args) {
         Calendar calendar = Calendar.getInstance();
         calendar.set(Calendar.YEAR, 2023);
         calendar.set(Calendar.MONTH, 6);
         calendar.set(Calendar.DATE, 7);
         calendar.set(Calendar.HOUR_OF_DAY, 13);
-        calendar.set(Calendar.MINUTE, 50);
+        calendar.set(Calendar.MINUTE, 16);
         calendar.set(Calendar.SECOND, 0);
 
         Date scheculeTime = calendar.getTime();
-        //GMT
+        // GMT
         System.out.println(scheculeTime);
-        //UTC
+        // UTC
         System.out.println(scheculeTime.toInstant());
 
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("実行しました！");
+                timer.cancel();
+            }
+        };
+
+        timer.schedule(task, scheculeTime);
     }
 
 }
