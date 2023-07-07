5c5a34a095dce803a78284ae9d38196bd0670920
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-07-07 11:21:07 +0900

    update

--- src/thread/CounterThread.java
+++ src/thread/CounterThread.java
@@ -6,12 +6,20 @@ public class CounterThread implements Runnable {
 
     @Override
     public void run() {
-        while (count > 0) {
-            String message = Thread.currentThread().getName() + ":" + count;
-            System.out.println(message);
-            count--;
+        synchronized (count) {
+            while (count > 0) {
+                try {
+                    Thread.sleep(interval);
+                    String message = Thread.currentThread().getName() + ":" + count;
+                    System.out.println(message);
+                    count--;
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            count = 5;
+            System.out.println("---End thread---");
         }
-        System.out.println("---End thread---");
     }
 
 }
