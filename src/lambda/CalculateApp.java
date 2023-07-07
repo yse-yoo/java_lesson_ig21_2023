5a1d5261ba9c36458caae5f4196bbca054d1241f
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-07-07 14:20:01 +0900

    update

--- src/lambda/CalculateApp.java
+++ src/lambda/CalculateApp.java
@@ -3,13 +3,18 @@ package lambda;
 public class CalculateApp {
     
     public static void main(String[] args) {
-        ICalculate calculate = new ICalculate() {
-            @Override
-            public Integer calculateWithoutTaxPrice(Integer price) {
-                Float withoutPrice = price / 1.1f;
-                return withoutPrice.intValue();
-            }
+        // ICalculate calculate = new ICalculate() {
+        //     @Override
+        //     public Integer calculateWithoutTaxPrice(Integer price) {
+        //         Float withoutPrice = price / 1.1f;
+        //         return withoutPrice.intValue();
+        //     }
+        // };
+        ICalculate calculate = (price)-> {
+            Float withoutPrice = price / 1.1f;
+            return withoutPrice.intValue();
         };
+
         Integer withoutTaxPrice = calculate.calculateWithoutTaxPrice(1000);
         System.out.println(withoutTaxPrice);
     }
