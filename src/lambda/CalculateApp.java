7a52baba1b0248ee87e7607e3386c8c3cfab19b5
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-07-07 13:47:36 +0900

    update

--- src/lambda/CalculateApp.java
+++ src/lambda/CalculateApp.java
@@ -4,13 +4,13 @@ public class CalculateApp {
     
     public static void main(String[] args) {
         ICalculate calculate = new ICalculate() {
-
             @Override
             public Integer calculateWithoutTaxPrice(Integer price) {
                 return 0;
             }
-            
         };
+        Integer withoutTaxPrice = calculate.calculateWithoutTaxPrice(1000);
+        System.out.println(withoutTaxPrice);
     }
 
 }
