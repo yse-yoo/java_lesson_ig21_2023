b29aae35dc4e75b6e6107ea41b247cfed585c1bc
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-07-07 10:53:53 +0900

    update

--- src/thread/CounterThread.java
+++ src/thread/CounterThread.java
@@ -1,7 +1,12 @@
 package thread;
 
-public class CounterThread {
+public class CounterThread implements Runnable {
     public static Integer count = 5;
     public static Integer interval = 1000;
+
+    @Override
+    public void run() {
+        System.out.println("---End thread---");
+    }
 
 }
