a6b788c30d884045f97a0216c75ca22be6a09a85
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-07-07 14:06:30 +0900

    update

--- src/lambda/RunnableApp.java
+++ src/lambda/RunnableApp.java
@@ -4,6 +4,13 @@ public class RunnableApp {
     public static void main(String[] args) {
         Runnable runner = ()-> System.out.println("Hello!");
         runner.run();
+
+        Runnable runner2 = ()-> {
+            System.out.println("Hello!");
+            System.out.println("GoodBye!");
+        };
+        runner2.run();
+
     }
 
 }
