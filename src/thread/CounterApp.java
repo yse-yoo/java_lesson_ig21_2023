0c9f4746c088ca56269810e8b33829d6f891fe88
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-07-07 10:57:50 +0900

    add thread

--- src/thread/CounterApp.java
+++ src/thread/CounterApp.java
@@ -2,6 +2,12 @@ package thread;
 
 public class CounterApp {
     public static void main(String[] args) {
-
+        CounterThread counter = new CounterThread();
+        Thread thread1 = new Thread(counter);
+        Thread thread2 = new Thread(counter);
+        thread1.setName("Thread1");
+        thread2.setName("Thread2");
+        thread1.start();
+        thread2.start();
     }
 }
