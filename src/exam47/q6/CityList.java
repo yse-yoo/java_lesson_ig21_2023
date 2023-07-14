9a7c88a3bf94a65f69b22fcf29beb5b0a5e55113
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-07-14 13:55:03 +0900

    add comment

--- src/exam47/q6/CityList.java
+++ src/exam47/q6/CityList.java
@@ -23,8 +23,11 @@ public class CityList {
                 // target: cities の index
                 // next: cities の index
                 for (int target = 0; target < length; target++) {
+                    //Debug
                     System.out.println("-- getDigit --");
-                    System.out.println(cities[target].getName() + "の" + place + "の位：" + getDigit(target, place));
+                    String message = cities[target].getName() + "の人口 " + place + " の位：" + getDigit(target, place);
+                    System.out.println(message);
+
                     if (getDigit(target, place) == digit) {
                         move(target, next);
                         next++;
